e9fd50f1b58030356bd2c83c42c594aeab62feb5
Tom Elliott <tmelliottjr@gmail.com>
2018-02-12 21:30:13 -0500

    add twilio keys and subscribe routes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+  return jsonify({'message': 'success'})
+
+@app.route('/unsubscribe', methods=['POST'])
+def unsubscribe():
+  return jsonify({'message':'success'})
+  
--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 
 class Config(object):
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'loose!lips!sink!ships'
+  TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+  TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
