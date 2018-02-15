3b59eb914441a55226e8bc96cac21747d047e3dc
Tom <tmelliottjr@gmail.com>
2018-02-14 19:59:09 -0500

    update routes

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,6 @@
 from flask import render_template, request, jsonify
 from app import app
 
-@app.route('/')
-@app.route('/index')
-def index():
-  return render_template('index.html')
-
 @app.route('/subscribe', methods=['POST'])
 def subscribe():
   return jsonify({'message': 'success'})
@@ -13,5 +8,4 @@ def subscribe():
 @app.route('/unsubscribe', methods=['POST'])
 def unsubscribe():
   return jsonify({'message':'success'})
-  
   
